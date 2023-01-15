15aa004d7d2579191a4188925be5578505af9abb
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2023-01-16 00:12:06 +0300

    addFileReact

---
+++ my-site/src/components/pages/Adverst.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./adverst.css";
+function Adverst() {
+  return (
+    <div className='adverst'>
+        <div className="adverstContainer">
+          <div className="adverstWrapper">
+            <h1 className="adverstTitle">EVERYTHING TO SELL ANYTHING</h1>
+            <span>Start your free website trial today.</span>
+            <span>No credit card required.</span>
+            <button className="getAdverstStarted">GET STARTED</button>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Adverst
